a42442d362ea73fa6f92df83d967011ae5e6d118
moneywait <94603475+moneywait@users.noreply.github.com>
2025-12-04 23:49:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const config = {
     introMessages: [
-        `今天是农历四月二十号，祝佳佳节日快乐~`, //第一句话
-        `关于节日礼物，我想<br/>老王就是你最好的礼物<br/>...`, //第二句话
-        `最后，我想换种方式：<br/><span class='highlight'>生日大抽奖</span><br/>由你自己来选择幸运礼物` //第三句话
+        `王樱迪是哈儿~`, //第一句话
+        `王樱迪是哈儿<br/>王樱迪是哈儿<br/>...`, //第二句话
+        `王樱迪是哈儿：<br/><span class='highlight'>王樱迪是哈儿奖</span><br/>王樱迪是哈儿` //第三句话
     ],
     emojiList: ['🎂', '🎉', '🎁', '🎈', '🍰', '🧧'], //生日特效的符号
-    contactPerson: '老魏', //兑奖人的姓名，比如你自己
+    contactPerson: '茶某', //兑奖人的姓名，比如你自己
     prizes: {
-        '奖项1': { description: '影视会员年卡 1 张', icon: '🎬' },
-        '奖项2': { description: '任意电影票 3 张', icon: '🎟️' },
-        '奖项3': { description: '生日红包 188 元', icon: '💸' },
-        '奖项4': { description: '结婚礼金+1000元', icon: '📸' },
-        '奖项5': { description: '帮实现 1 个愿望', icon: '🌠' },
-        '奖项6': { description: '增加 1 次抽奖机会', icon: '🔁' }
+        '奖项1': { description: '欢快的歌 ', icon: '🎬' },
+        '奖项2': { description: '伤心的歌', icon: '🎟️' },
+        '奖项3': { description: 'R&B', icon: '💸' },
+        '奖项4': { description: '爵士', icon: '📸' },
+        '奖项5': { description: '周杰伦', icon: '🌠' },
+        '奖项6': { description: '李荣浩', icon: '🔁' }
     }
 };
